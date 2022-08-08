2472192119b6c3417b52c7133c9a673dd749e4e4
sergiokhark <sergio.khark@gmail.com>
2022-08-08 15:42:37 +0300

    pagination

--- pages/users.vue
+++ pages/users.vue
@@ -10,7 +10,7 @@
                     <td>Phone</td>
                     <td>e-mail</td>
                 </tr>
-                <tr v-for="(user, index) in users" :key="index">
+                <tr v-for="(user, index) in paginatedUser" :key="index">
                     <td>
                         {{index + 1}}
                     </td>
@@ -29,6 +29,11 @@
                 </tr>
             </table>
         </div>
+        <div class="table-pagination">
+            <div class="page" v-for="(page, index) in pages" :key="index" @click="pageClick(page)">
+                {{page}}
+            </div>
+        </div>
     </div>
 </template>
 
@@ -38,13 +43,30 @@ import axios from 'axios'
 export default {
     data() {
         return {
-            users: []
+            users: [],
+            pageNumber: 1,
+            userPerPage: 10,
+        }
+    },
+    computed: {
+        pages() {
+            return Math.ceil(this.users.length / this.userPerPage)
+        },
+        paginatedUser() {
+            let from = (this.pageNumber - 1) * this.userPerPage
+            let to = from + this.userPerPage
+            return this.users.slice(from, to)
         }
     },
     mounted() {
         axios.get('https://run.mocky.io/v3/0b90eddc-eb9b-461d-9974-81c4cac6e277')
         .then(response => (this.users = response.data.data))
         //console.log(this.users)
+    },
+    methods: {
+        pageClick(page) {
+            this.pageNumber = page
+        }
     }
 
 }
@@ -76,6 +98,19 @@ export default {
     .content {
         max-width: 900px;
         margin: 0px auto;
+    }
+    .table-pagination {
+        display: flex;
+        justify-content: center;
+        margin-top: 20px;
+    }
+    .page {
+        padding: 8px;
+        border: 1px solid #000;
+        margin: 0px 5px 0px 0px
+    }
+    .page:hover {
+        background: #e7e7e7;
     }
 
 </style>
