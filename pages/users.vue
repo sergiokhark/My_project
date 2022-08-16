8f2d3dd89e4a78beee418344819bb1c06b66cfed
sergiokhark <sergio.khark@gmail.com>
2022-08-16 14:55:48 +0300

    Auth/Layout/Add/Edit/Search/

--- pages/users.vue
+++ pages/users.vue
@@ -1,51 +1,294 @@
 <template>
-    <v-app>
-        <v-data-table
-            dense
-            :headers="headers"
-            :items="users"
-            :items-per-page="5"
-            class="elevation-1"
-        ></v-data-table>
-    </v-app>
-        
+  <v-data-table
+    :headers="headers"
+    :items="users"
+    :search="search"
+    :items-per-page="5"
+    class="elevation-1"
+  >
+    <template v-slot:top>
+      <v-toolbar
+        flat
+      >
+        <v-toolbar-title>Users data</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+        <v-spacer></v-spacer>
+        <v-dialog
+          v-model="dialog"
+          max-width="650px"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              color="primary"
+              dark
+              class="mb-2"
+              v-bind="attrs"
+              v-on="on"
+            >
+              Add user
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">{{ formTitle }}</span>
+            </v-card-title>
+
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.id"
+                      label="id"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.name"
+                      label="Name"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.username"
+                      label="Username"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.email"
+                      label="Email"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.address.city"
+                      label="City"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.address.street"
+                      label="Street"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.website"
+                      label="Site"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    sm="6"
+                    md="4"
+                  >
+                    <v-text-field
+                      v-model="editedItem.phone"
+                      label="Phone"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="blue darken-1"
+                text
+                @click="close"
+              >
+                Cancel
+              </v-btn>
+              <v-btn
+                color="blue darken-1"
+                text
+                @click="save"
+              >
+                Save
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5">Are you sure?</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template v-slot:[`item.actions`]="{ item }">
+      <v-icon
+        small
+        class="mr-2"
+        @click="editItem(item)"
+      >
+        mdi-pencil
+      </v-icon>
+      <v-icon
+        small
+        @click="deleteItem(item)"
+      >
+        mdi-delete
+      </v-icon>
+    </template>
+    
+  </v-data-table>
 </template>
 
 <script>
+
 import axios from 'axios'
 
 export default {
-    data() {
-        return {
-            users: [],
-            headers: [
-                { text: 'Firstname', value: 'firstname' },
-                { text: 'Lastname', value: 'lastname' },
-                { text: 'Phone prefix', value: 'telephone.prefix' },
-                { text: 'Phone number', value: 'telephone.number' },
-                { text: 'Email', value: 'email' },
-                { text: 'Date of birthday', value: 'dob' },
-                { text: 'ID', value: 'id' },
-            ],
-        }
-    },
-    middleware: 'auth',
-    layout: 'login',
-    mounted() {
-        axios.get('https://run.mocky.io/v3/31fafaf5-cc55-4cad-a945-ea2b9be78e3a')
-        .then(response => (this.users = response.data.data))
-    },
-    computed: {
-        
-    },
-    methods: {
-        
+  data() {
+    return {
+      headers: [
+        { text: 'id', value: 'id' },
+        { text: 'Name', value: 'name' },
+        { text: 'Username', value: 'username' },
+        { text: 'Email', value: 'email' },
+        { text: 'City', value: 'address.city' },
+        { text: 'Street', value: 'address.street' },
+        { text: 'Site', value: 'website' },
+        { text: 'Phone', value: 'phone' },
+        { text: 'Actions', value: 'actions', sortable: false },
+      ],
+      search: '',
+      users: [],
+      dialog: false,
+      dialogDelete: false,
+      editedIndex: -1,
+      editedItem: {
+        id: '',
+        name: '',
+        username: '',
+        email: '',
+        address: {
+          city: '',
+          street: '',
+        },
+        website: '',
+        phone: '',
+      },
+      defaultItem: {
+        id: '',
+        name: '',
+        username: '',
+        email: '',
+        address: {
+          city: '',
+          street: '',
+        },
+        website: '',
+        phone: '',
+      },
     }
+  },
+  middleware: 'auth',
+  layout: 'login',
+  mounted() {
+    axios.get('https://run.mocky.io/v3/f2c2e2f8-0de4-446d-af5d-52895b8064f9')
+    .then(response => (this.users = response.data))
 
+  },
+  computed: {
+    
+    formTitle () {
+        return this.editedIndex === -1 ? 'New User' : 'Edit User'
+    },
+  },
+  methods: {
+    editItem (item) {
+        this.editedIndex = this.users.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialog = true
+    },
+    deleteItem (item) {
+        this.editedIndex = this.users.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialogDelete = true
+    },
+    deleteItemConfirm () {
+        this.users.splice(this.editedIndex, 1)
+        this.closeDelete()
+    },
+    close () {
+        this.dialog = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+    },
+    closeDelete () {
+        this.dialogDelete = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+    },
+    save () {
+        if (this.editedIndex > -1) {
+          Object.assign(this.users[this.editedIndex], this.editedItem)
+        } else {
+          this.users.push(this.editedItem)
+        }
+        this.close()
+    },
+  },
+  watch: {
+      dialog (val) {
+        val || this.close()
+      },
+      dialogDelete (val) {
+        val || this.closeDelete()
+      },
+    },
 }
 </script>
 
 
 <style scoped>
-    
 </style>
